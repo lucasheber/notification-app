6ac757679d669d0d61b52ad4d229527de8d8625a
Lucas Heber <lucas.heber07@gmailcom>
2018-11-07 21:27:33 -0200

    [del] all files

--- public/firebase-messaging-sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("/__/firebase/5.5.5/firebase-app.js");
-importScripts("/__/firebase/5.5.5/firebase-messaging.js");
-importScripts("/__/firebase/init.js");
-
-const messaging = firebase.messaging();
