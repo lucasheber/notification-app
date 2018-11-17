e7f830a51ecb43b3095c5964474020c257e418fe
Lucas Heber <lucas.heber07@gmailcom>
2018-11-16 23:49:30 -0200

    [up] event click

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 
 self.addEventListener('fetch', (event) => {})
 
-// self.addEventListener('notificationclick', function(event) {
-//   console.log('[Service Worker] Notification click Received.');
-//
-//   event.notification.close();
-//
-//   event.waitUntil(
-//     clients.openWindow('https://notification-app-7da05.firebaseapp.com')
-//   );
-// })
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://notification-app-7da05.firebaseapp.com')
+  );
+})
